6722d15d9dac611d5ba9a5048d847f87a570b2d7
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-08-16 14:57:44 -0300

    Improves size tests in multiple ways
    Makes them more accurate: multiples of iterators, not pointers.
    Makes them rely on types instead of values.

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <iostream>
+#include <type_traits>
 #include <vector>
 
 #include "Algorithm.hpp"
@@ -24,10 +25,10 @@ BOOST_AUTO_TEST_CASE(test_range_with_empty_vector) {
   BOOST_TEST(v == toVector(makeRange(v)));
 }
 
-BOOST_AUTO_TEST_CASE(test_range_over_vector_should_use_the_same_space_as_2_pointers) {
-  const auto v = std::vector<S32>{};
-  const auto range = makeRange(v);
-  BOOST_TEST(2 * sizeof(void *) == sizeof(range));
+BOOST_AUTO_TEST_CASE(test_range_over_vector_should_use_the_same_space_as_2_iterators) {
+  using Vector = std::vector<S32>;
+  using Iterator = Vector::iterator;
+  BOOST_TEST(2 * sizeof(Iterator) == sizeof(std::invoke_result<decltype(makeRange<Vector>), Vector>::type));
 }
 
 BOOST_AUTO_TEST_CASE(test_range_with_non_empty_vector) {
@@ -40,10 +41,13 @@ BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_1_with_empty_vector) {
   BOOST_TEST(v == toVector(makeDeepRange<1>(v)));
 }
 
-BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_1_over_vector_should_use_the_same_space_as_2_pointers) {
-  const auto v = std::vector<S32>{};
-  const auto range = makeDeepRange<1>(v);
-  BOOST_TEST(2 * sizeof(void *) == sizeof(range));
+BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_1_over_vector_should_use_the_same_space_as_2_iterators) {
+  using Vector = std::vector<S32>;
+  using Iterator = Vector::iterator;
+  BOOST_TEST(2 * sizeof(Iterator) == sizeof(std::invoke_result<decltype(makeDeepRange<1, Vector>), Vector>::type));
+  using VectorOfVectors = std::vector<std::vector<S32>>;
+  using VectorOfVectorsIterator = VectorOfVectors::iterator;
+  BOOST_TEST(2 * sizeof(VectorOfVectorsIterator) == sizeof(std::invoke_result<decltype(makeDeepRange<1, VectorOfVectors>), VectorOfVectors>::type));
 }
 
 BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_2_with_empty_vector) {
@@ -52,10 +56,10 @@ BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_2_with_empty_vector) {
   BOOST_TEST(emptyVector == toVector(makeDeepRange<2>(v)));
 }
 
-BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_2_over_vector_should_use_the_same_space_as_4_pointers) {
-  const auto v = std::vector<std::vector<S32>>{};
-  const auto range = makeDeepRange<2>(v);
-  BOOST_TEST(4 * sizeof(void *) == sizeof(range));
+BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_2_over_vector_should_use_the_same_space_as_4_iterators) {
+  using Vector = std::vector<std::vector<S32>>;
+  using Iterator = Vector::iterator;
+  BOOST_TEST(4 * sizeof(Iterator) == sizeof(std::invoke_result<decltype(makeDeepRange<2, Vector>), Vector>::type));
 }
 
 BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_2_over_non_empty_vector_of_empty_vectors) {
@@ -94,10 +98,10 @@ BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_3_with_empty_vector) {
   BOOST_TEST(emptyVector == toVector(makeDeepRange<3>(v)));
 }
 
-BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_3_over_vector_should_use_the_same_space_as_6_pointers) {
-  const auto v = std::vector<std::vector<std::vector<S32>>>{};
-  const auto range = makeDeepRange<3>(v);
-  BOOST_TEST(6 * sizeof(void *) == sizeof(range));
+BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_3_over_vector_should_use_the_same_space_as_6_iterators) {
+  using Vector = std::vector<std::vector<std::vector<S32>>>;
+  using Iterator = Vector::iterator;
+  BOOST_TEST(6 * sizeof(Iterator) == sizeof(std::invoke_result<decltype(makeDeepRange<3, Vector>), Vector>::type));
 }
 
 BOOST_AUTO_TEST_CASE(test_deep_range_of_depth_4_over_empty_list) {
